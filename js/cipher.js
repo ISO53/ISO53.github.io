8c92f21bc4078f6b7522781d006b8a60fb03cb76
ISO53 <102249575+ISO53@users.noreply.github.com>
2023-09-07 13:46:18 +0300

    index.html adjusted to be mobile friendly

--- js/cipher.js
+++ js/cipher.js
@@ -19,22 +19,54 @@ function wheelListener() {
 		let wheelCenter = getCenter(wheel);
 		window.addEventListener("resize", () => (wheelCenter = getCenter(wheel)));
 
+		// Mouse events
 		wheel.addEventListener("mousedown", () => (isClicked[i] = true));
-		document.addEventListener("mousemove", (event) => {
+		document.addEventListener("mousemove", handleMouseMove);
+
+		// Touch events
+		wheel.addEventListener("touchstart", (event) => {
+			event.preventDefault(); // Prevent the default touch behavior (e.g., scrolling)
+			isClicked[i] = true;
+		});
+		document.addEventListener("touchmove", handleTouchMove);
+
+		function handleMouseMove(event) {
 			if (!isClicked[i]) {
 				return;
 			}
 
 			const angle = Math.atan2(event.clientY - wheelCenter.y, event.clientX - wheelCenter.x);
 			wheel.style.transform = `rotate(${angle}rad)`;
-		});
+		}
+
+		function handleTouchMove(event) {
+			if (!isClicked[i] || event.touches.length !== 1) {
+				return;
+			}
+
+			const touch = event.touches[0];
+			const angle = Math.atan2(touch.clientY - wheelCenter.y, touch.clientX - wheelCenter.x);
+			wheel.style.transform = `rotate(${angle}rad)`;
+		}
 	}
 
+	// Mouse events
 	document.addEventListener("mouseup", handleMouseUp);
+
+	// Touch events
+	document.addEventListener("touchend", handleTouchEnd);
 
 	function handleMouseUp() {
 		isClicked.fill(false);
+		checkPassword();
+	}
 
+	function handleTouchEnd() {
+		isClicked.fill(false);
+		checkPassword();
+	}
+
+	function checkPassword() {
 		let counter = 0;
 		for (let i = 0; i < password.length; i++) {
 			const lt = password[i];
@@ -47,8 +79,9 @@ function wheelListener() {
 		}
 
 		if (counter === 5) {
-			console.log("password correct!");
+			console.log("Password correct!");
 			document.removeEventListener("mouseup", handleMouseUp);
+			document.removeEventListener("touchend", handleTouchEnd);
 
 			let menuButton = document.getElementById("menu_button");
 
@@ -56,31 +89,31 @@ function wheelListener() {
 			menuButton.click();
 		}
 	}
+}
 
-	function getCenter(element) {
-		const { left, top, width, height } = element.getBoundingClientRect();
-		return { x: left + width / 2, y: top + height / 2 };
-	}
+function getCenter(element) {
+	const { left, top, width, height } = element.getBoundingClientRect();
+	return { x: left + width / 2, y: top + height / 2 };
+}
 
-	function getRotation(element) {
-		const computedStyle = window.getComputedStyle(element);
-		const transformValue = computedStyle.getPropertyValue("transform");
-		let rotateValueInRad;
+function getRotation(element) {
+	const computedStyle = window.getComputedStyle(element);
+	const transformValue = computedStyle.getPropertyValue("transform");
+	let rotateValueInRad;
 
-		if (transformValue && transformValue !== "none") {
-			// Extract the matrix values from the transform string using regex
-			const match = transformValue.match(/^matrix\((.+)\)$/);
-			if (match && match.length >= 2) {
-				// Split the matrix values into an array
-				const matrixValues = match[1].split(", ");
+	if (transformValue && transformValue !== "none") {
+		// Extract the matrix values from the transform string using regex
+		const match = transformValue.match(/^matrix\((.+)\)$/);
+		if (match && match.length >= 2) {
+			// Split the matrix values into an array
+			const matrixValues = match[1].split(", ");
 
-				// Calculate the rotation in radians using the matrix values
-				const a = parseFloat(matrixValues[0]);
-				const b = parseFloat(matrixValues[1]);
-				rotateValueInRad = Math.atan2(b, a);
-			}
+			// Calculate the rotation in radians using the matrix values
+			const a = parseFloat(matrixValues[0]);
+			const b = parseFloat(matrixValues[1]);
+			rotateValueInRad = Math.atan2(b, a);
 		}
-
-		return rotateValueInRad;
 	}
+
+	return rotateValueInRad;
 }
