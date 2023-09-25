3c09470ebc37471b947fb14090d83a43fef39fe2
ISO53 <102249575+ISO53@users.noreply.github.com>
2023-09-25 19:20:15 +0300

    added skip button to cipher for losers

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@
 
 // ************************ JS Starts ************************
 firstLoadAnimations();
+skipButtonListener();
 
 // ******************** Declare Functions ********************
 function firstLoadAnimations() {
 	setTimeout(() => document.getElementById("welcome").classList.remove("hide"), 500);
 	setTimeout(() => document.getElementById("cipher").classList.remove("hide"), 1000);
+	setTimeout(() => document.getElementById("red_button").classList.remove("hide"), 1500);
 
 	setTimeout(() => (document.getElementById("wheel_1").style.transform = "rotate(30deg)"), 1000);
 	setTimeout(() => (document.getElementById("wheel_2").style.transform = "rotate(-60deg)"), 1000);
@@ -19,4 +21,15 @@ function firstLoadAnimations() {
 	setTimeout(() => document.getElementById("wheel_3").classList.remove("wheel_transition"), 4000);
 	setTimeout(() => document.getElementById("wheel_4").classList.remove("wheel_transition"), 4000);
 	setTimeout(() => document.getElementById("wheel_5").classList.remove("wheel_transition"), 4000);
+}
+
+function skipButtonListener() {
+	let skipButton = document.getElementById("red_button");
+
+	skipButton.onclick = () => {
+		let menuButton = document.getElementById("menu_button");
+
+		menuButton.classList.remove("hide_button");
+		menuButton.click();
+	};
 }
