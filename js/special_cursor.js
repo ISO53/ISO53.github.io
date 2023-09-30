c26b75e2d0931a8348b42964be8fd4ea86fed658
ISO53 <102249575+ISO53@users.noreply.github.com>
2023-09-30 16:56:42 +0300

    special cursor changed for better visual

--- js/special_cursor.js
+++ js/special_cursor.js
@@ -1,40 +1,72 @@
 // ******************** Declare Variables ********************
 
 // ************************ JS Starts ************************
-cursorAnimation();
+cursorAnimationCircle();
+cursorAnimationDot();
 
 // ******************** Declare Functions ********************
-function cursorAnimation() {
-	document.addEventListener("DOMContentLoaded", () => {
-		let mousePosX = 0;
-		let mousePosY = 0;
-		let mouseCircle = document.getElementById("mouse_circle");
-		let timeout;
+function cursorAnimationCircle() {
+	let mousePosX = 0;
+	let mousePosY = 0;
+	let mouseCircle = document.getElementById("mouse_circle");
+	let timeout;
 
-		document.onmousemove = (e) => {
-			mousePosX = e.pageX;
-			mousePosY = e.pageY;
+	document.addEventListener("mousemove", (e) => {
+		mousePosX = e.pageX;
+		mousePosY = e.pageY;
 
-			mouseCircle.style.opacity = "1";
+		mouseCircle.style.opacity = "1";
 
-			clearTimeout(timeout);
-			timeout = setTimeout(() => (mouseCircle.style.opacity = "0"), 1000);
-		};
-
-		let delay = 6;
-		let revisedMousePosX = 0;
-		let revisedMousePosY = 0;
-
-		function delayMouseFollow() {
-			requestAnimationFrame(delayMouseFollow);
-
-			revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
-			revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
-
-			mouseCircle.style.top = revisedMousePosY + "px";
-			mouseCircle.style.left = revisedMousePosX + "px";
-		}
-
-		delayMouseFollow();
+		clearTimeout(timeout);
+		timeout = setTimeout(() => (mouseCircle.style.opacity = "0"), 1000);
 	});
+
+	let delay = 4;
+	let revisedMousePosX = 0;
+	let revisedMousePosY = 0;
+
+	function delayMouseFollow() {
+		requestAnimationFrame(delayMouseFollow);
+
+		revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
+		revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
+
+		mouseCircle.style.top = revisedMousePosY + "px";
+		mouseCircle.style.left = revisedMousePosX + "px";
+	}
+
+	delayMouseFollow();
+}
+
+function cursorAnimationDot() {
+	let mousePosX = 0;
+	let mousePosY = 0;
+	let mouseCircle = document.getElementById("mouse_dot");
+	let timeout;
+
+	document.addEventListener("mousemove", (e) => {
+		mousePosX = e.pageX;
+		mousePosY = e.pageY;
+
+		mouseCircle.style.opacity = "1";
+
+		clearTimeout(timeout);
+		timeout = setTimeout(() => (mouseCircle.style.opacity = "0"), 1000);
+	});
+
+	let delay = 1;
+	let revisedMousePosX = 0;
+	let revisedMousePosY = 0;
+
+	function delayMouseFollow() {
+		requestAnimationFrame(delayMouseFollow);
+
+		revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
+		revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
+
+		mouseCircle.style.top = revisedMousePosY + "px";
+		mouseCircle.style.left = revisedMousePosX + "px";
+	}
+
+	delayMouseFollow();
 }
